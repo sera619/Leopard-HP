af414e88f417cf7aa59a4506ae44940870858365
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-07-20 04:27:43 +0200

    create payment blueprint

---
+++ leos/payment.py
@@ -0,0 +1,14 @@
+from crypt import methods
+import os
+import logging
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, Blueprint
+from flask.helpers import url_for
+
+load_dotenv()
+
+payment = Blueprint('payment', __name__)
+
+@payment.route('/spend-confirm', methods=["POST", "GET"])
+def spend_confirm():
+    pass
