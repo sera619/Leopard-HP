4b38fec6b37bfeebbe284267cfadea5635719c68
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-07-20 20:36:02 +0200

    add animalcards & video | style fixes

--- leos/main.py
+++ leos/main.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template, flash, request, send_file
 from flask.helpers import url_for
 from werkzeug.utils import redirect
 from dotenv import load_dotenv
+from .util import WebTexts
 
 
 load_dotenv()
@@ -29,8 +30,23 @@ def entstehung():
 
 @main.route("/spenden", methods=["GET", "POST"])
 def spend():
+    card1_title = WebTexts.OWLTITLE
+    card1_text = WebTexts.OWLTEXT
+    card1_image_url = WebTexts.OWLIMAGEURL
+
+    card2_title = WebTexts.DOGTITLE
+    card2_text = WebTexts.DOGTEXT
+    card2_image_url = WebTexts.DOGIMAGEURL
     logging.info("Besucher auf Spenden-Seite")
-    return render_template("spends.html", site_title="Seniorenresidenz für Showtiere | Spenen & Patenschaft")
+    return render_template("spends.html",
+     site_title="Seniorenresidenz für Showtiere | Spenen & Patenschaft",
+     card_1_text = card1_text,
+     card_1_title = card1_title,
+     card_1_imageURL = card1_image_url,
+     card_2_title = card2_title,
+     card_2_text = card2_text,
+     card_2_imageURL = card2_image_url
+     )
 
 @main.route("/tiere", methods=["POST", "GET"])
 def unsere_tiere():
---
+++ leos/util.py
@@ -0,0 +1,37 @@
+
+
+
+class WebTexts: 
+    OWLTITLE = "Ika(Rus),\ndie Schleiereule"
+    OWLIMAGEURL = "../static/images/animals/owl.jpg"
+
+    OWLTEXT = """Update: behördlich vermittelt in einen Tierpark  - Wir Danken den Futterspendern!
+Die Behörde hat die Tierarztkosten doch noch bezahlt!\n\nStand 18.01.2022\nAm 08.01.2022 habe ich den Kampf gegen meinen Revierrevalen in freier Wildbahn verloren und fiel wie ein Stein vom Himmel. Menschen beobachteten dieses Drama, entnahmen mich der Natur, brachten mich in die Seniorenresidenz für Showtiere. Wenige Stunden später befand ich mich in einer Spezialklinik für Vögel. Die Ärzte bemühen sich, mein rechtes Auge zu retten.\n
+Heute bekam ich  von den Behörden schlechte Nachrichten.  Es konnte für mich kein Platz in einem Tierpark oder Zoo durch die Behörden gefunden werden. Auch kann die Behörde die Kosten für die bisherige Behandling und der bevorstehenden OP, nämlich der Entfernung meines rechten Auges in Höhe von  ca 2500,00€ nicht zahlen, das Budget gibt das nicht her.
+Wie trauig ist das, denn der Tierschutz ist doch ein Staatsziel und im Grundgesetz verankert. Findet sich niemand, die Kosten zu tragen, muss ich wohl meinen letzten Flug antreten.\nICH MÖCHTE ABER LEBEN!!!! Ich fresse auch selbstständig meine Mäuse und Küken.
+
+Die Tierärzte versuchen alles, um mir zu helfen, aber was tut der Staat für mich? Ich bin doch SEIN WILDTIER!  In der Seniorenresidenz für Showtiere dürfte ich mit Sondergenehmigung bleiben, denn die Fach- und Sachkunde haben die Behörden bereits anerkannt.
+
+
+Ich habe Euer Herz erreicht und Ihr wollt mir und den anderen Tieren helfen? Dann nutzt Paypal (tiger_bs@gmx.de - Stichwort Eule oder Ika(Rus) oder ruft an. Mir und den anderen Tieren hilft jeder Euro. Zum Dank schicke ich Euch auch gerne eine gemauserte Feder, wenn Ihr wollt. (Adresse bitte nicht vergessen!)
+
+Über meinen Gesundheitszustand Informiert Euch Birgit gerne unter 0171/22 82 546"""
+    DOGTITLE = " Max Mischling"
+    DOGIMAGEURL = "../static/images/animals/dog.jpg"
+
+    DOGTEXT = """Stand 18.01.22 
+Max Mischling ist seit fast 9 Monaten bei uns und ist ein glücklicher Seniorenhund. Mit seinen fast 18 Jahren hat er sein Rudel gefunden. Auch sein Futterpate freut sich über die tolle Entwicklung. Leider kommt manchmal die Altersdemenz, dann vergisst er schon mal draussen Pipi zu machen. Seine Geschichte lest Ihr nachfolgend.
+
+21.04.2021
+
+16.00 Uhr das Telefon klingelt. Eine befreundete Tierärztin ruft an und erzählt mir die traurigste Geschichte, die ich seit langem gehört habe. Max ist 17 Jahre und sein Frauchen hatte im Dezember mit 89 Jahren einen Unfall. Ist seit dem bettlegerisch und wird von der Familie gepflegt. So wie das Leben spielt, muss nun aber die pflegende Tochter selber ins Krankenhaus und die alte Dame somit ins Heim. Aber da ist noch Max. Was wird jetzt aus ihm? Die Familie sucht Hilfe, ruft in verschiedenen Tierheimen an, doch keiner will Max aufnehmen. Zu Alt, hinkend und dann noch inkontinent. Keine Aussichten auf Vermittlung.....
+Schweren Herzens entschließt sich die Familie Max für immer gehen zu lassen. 
+Sie gehen zu besagter Tierärztin. Diese stellt Herzprobleme fest, Wasseransammlungen im Körper, doch für den letzten Gang ist er einfach noch zu fit. 
+Eine halbe Stunde später bin ich eingetroffen. Mich begrüßte ein freundlicher halb blinder, und schwer hörender kleiner Kerl, der sicherlich noch ein paar schöne Wochen, vielleicht auch Monate vor sich haben kann.
+Max ist somit in die Seniorenresidenz für Showtiere aufgenommen. Er ist kein Star, doch er erfüllt das wichtigste Aufnahmekriterium. Bei uns finden Tiere ein Zu Hause, die nicht von Tierheimen aufgenommen werden....Max benötigt in seinem Alter und mit seinen Erkrankungen spezielles Futter und teuere Medikamente.
+Und nun kommt die Frage aller Fragen.... MÖCHTEN SIE  SEIN PATE SEIN? 
+Sie wollen helfen? Kontaktieren Sie uns per Mail oder auch Paypal unter tiger_bs@gmx.de 
+Wie es weiter geht mit Max? - Sie erfahren es bei uns! 
+
+    
+    """
